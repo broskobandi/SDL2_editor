3082d07d036c38a57181d81dc4669f11671c4187
broskobandi <broskobandi@protonmail.com>
2025-11-04 15:01:21 +0000

    Add thumbnails struct

--- src/browser.hpp
+++ src/browser.hpp
@@ -4,7 +4,6 @@
 #define BROWSER_HPP
 
 #include "core.hpp"
-#include <cstddef>
 #include <cstdlib>
 #include <string>
 #include <utility>
@@ -17,11 +16,16 @@ class Browser {
 
 private:
 
+	struct Thumbnail {
+		SDL_Rect rect;
+		std::string path_to_bmp;
+		bool is_highlighted = {false};
+	};
+
 	float panel_width_multiplier;
 	SDL_Color panel_col;
 	SDL_Rect panel;
-	std::vector<std::string> paths_to_bmps;
-	std::vector<SDL_Rect> thumbnails;
+	std::vector<Thumbnail> thumbnails;
 	int thumbnails_offset {0};
 
 	void set_panel_size(std::pair<int, int> win_size) {
@@ -36,24 +40,27 @@ private:
 			throw std::runtime_error("Thumbnails vector is empty.");
 		int i = 0;
 		for (auto& t : thumbnails) {
-			t.x = 0;
-			t.y = panel.w * i + thumbnails_offset;
-			t.w = panel.w;
-			t.h = panel.w;
+			t.rect.x = 0;
+			t.rect.y = panel.w * i + thumbnails_offset;
+			t.rect.w = panel.w;
+			t.rect.h = panel.w;
 			i++;
 		}
 	}
+
+	// void highlight_thumbnail(std::pair<int,int> mouse_pos) {
+	//
+	// }
 
 public:
 
 	void update(std::pair<int, int> win_size, int scroll_speed, std::pair<int,int> mouse_pos) {
 		if (mouse_pos.first < panel.w)
 			thumbnails_offset += scroll_speed;
-		if (thumbnails.front().y > 0)
-			// thumbnails_offset--;
+		if (thumbnails.front().rect.y > 0)
+			//let's not hardcode these..
 			thumbnails_offset -= 5;
-		if (thumbnails.back().y < panel.h - panel.w)
-			// thumbnails_offset++;
+		if (thumbnails.back().rect.y < panel.h - panel.w)
 			thumbnails_offset += 5;
 		if (std::abs(thumbnails_offset) <= 5)
 			thumbnails_offset = 0;
@@ -78,12 +85,17 @@ public:
 			for (const auto& entry : dir_iter) {
 				auto s = entry.path().string();
 				if (s.substr(s.length() - 4, 4) == ".bmp") {
-					paths_to_bmps.push_back(s);
-					thumbnails.push_back({0, 0, 0, 0});
+					// paths_to_bmps.push_back(s);
+					Thumbnail t;
+					t.path_to_bmp = s;
+					t.rect = {0, 0, 0, 0};
+					thumbnails.push_back(t);
+					// thumbnails.push_back({0, 0, 0, 0});
 				}
 			}
 
-			DBGMSG(paths_to_bmps.size() << " bmp files found in directory.");
+			// DBGMSG(paths_to_bmps.size() << " bmp files found in directory.");
+			DBGMSG(thumbnails.size() << " bmp files found in directory.");
 
 		} catch (const std::filesystem::filesystem_error&) {
 			throw std::runtime_error("Invalid path.");
@@ -104,20 +116,23 @@ public:
 
 		// Thumbnails
 
-		int i = 0;
+		// int i = 0;
 		for (const auto& t : thumbnails) {
 			RenderData thumbnail;
-			thumbnail.dstrect = t;
-			thumbnail.col_or_path_to_tex = paths_to_bmps.at(static_cast<std::size_t>(i));
+			thumbnail.dstrect = t.rect;
+			thumbnail.col_or_path_to_tex = t.path_to_bmp;
 			data.push_back(thumbnail);
-			i++;
 		}
 
 		return data;
 	}
 
 	auto get_paths_to_bmps() {
-		return paths_to_bmps;
+		std::vector<std::string> paths;
+		for (const auto& t: thumbnails) {
+			paths.push_back(t.path_to_bmp);
+		}
+		return paths;
 	}
 };
 
